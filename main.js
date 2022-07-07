e6f771483b6d86bdebb5053cc08e3790fc586d18
ankleBowl <specialreeree6@gmail.com>
2022-07-06 17:12:12 -0700

    Added rycorp page

---
+++ main.js
@@ -0,0 +1,17 @@
+// register the onscroll event
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (currentScrollPos > 200) {
+        var arrow = document.getElementById("downarrow");
+        if (arrow.classList.contains("fadeIn")) {
+            arrow.classList.remove("fadeIn");
+            arrow.classList.add("fadeOut");
+        }
+    } else {
+        var arrow = document.getElementById("downarrow");
+        if (arrow.classList.contains("fadeOut")) {
+            arrow.classList.add("fadeIn");
+            arrow.classList.remove("fadeOut");
+        }
+    }
+}
